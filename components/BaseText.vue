6a91ce053f98d8af28385ae9335e86767407649f
AxelRSa <axelg.rizos@gmail.com>
2022-09-23 14:55:48 -0500

    feat(text): add new fonts from googleFonts
    and use by default

--- components/BaseText.vue
+++ components/BaseText.vue
@@ -1,5 +1,25 @@
 <template>
-  <span v-bind="$attrs" v-on="$listeners">
+  <span :is="tag" class="text" v-bind="$attrs" v-on="$listeners">
     <slot />
   </span>
 </template>
+
+<script>
+export default {
+  props: {
+    tag: { type: String, default: 'span' }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.text{
+  &.italiana{
+    font-family: 'Italiana', serif;
+  }
+
+  &.background-black-color-white{
+    color: var(--background-black_text-white)
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,11 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // italiana font
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Italiana&family=Roboto:wght@300&display=swap' }
     ]
   },
 
