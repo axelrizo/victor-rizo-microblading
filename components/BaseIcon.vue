5341ab8d8102b678e9eacc4958144472feed7270
AxelRSa <axelg.rizos@gmail.com>
2022-09-23 12:07:14 -0500

    feat(components): add base components and icons

---
+++ components/BaseIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="base-icon">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.base-icon{
+  height: 20px;
+  width: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ components/BaseLogo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="base-logo">
+    <BaseImage :src="require('~/assets/images/logo.jpg')" alt="microblading cdmx logo" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.base-logo{
+  height: 40px;
+  width: auto;
+}
+</style>
