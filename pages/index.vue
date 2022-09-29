2b027863326a3b7add04024362daad046a55d6d6
AxelRSa <axelg.rizos@gmail.com>
2022-09-29 09:11:18 -0500

    feat(hero): add swiper example components

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,49 @@
 <template>
-  <Tutorial />
+  <div>
+    <swiper class="mySwiper">
+      <swiper-slide class="h-20">
+        Slide 1
+      </swiper-slide>
+      <swiper-slide class="h-20">
+        Slide 2
+      </swiper-slide>
+      <swiper-slide class="h-20">
+        Slide 3
+      </swiper-slide>
+      <swiper-slide class="h-20">
+        Slide 4
+      </swiper-slide>
+      <swiper-slide class="h-20">
+        Slide 5
+      </swiper-slide>
+      <swiper-slide class="h-20">
+        Slide 6
+      </swiper-slide>
+      <swiper-slide class="h-20">
+        Slide 7
+      </swiper-slide>
+      <swiper-slide class="h-20">
+        Slide 8
+      </swiper-slide>
+      <swiper-slide class="h-20">
+        Slide 9
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import 'swiper/swiper-bundle.css'
+import { Navigation, Pagination } from 'swiper'
+import { SwiperCore, Swiper, SwiperSlide } from 'swiper-vue2'
 
-export default Vue.extend({})
+SwiperCore.use([Navigation, Pagination])
+
+export default Vue.extend({
+  components: {
+    Swiper,
+    SwiperSlide
+  }
+})
 </script>
