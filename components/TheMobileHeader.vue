0de75df9428016e26c16fbc3dd91183f1bcd143f
AxelRSa <axelg.rizos@gmail.com>
2022-09-29 09:09:11 -0500

    feat(accessibility): change header buttons sizes

--- components/TheMobileHeader.vue
+++ components/TheMobileHeader.vue
@@ -1,17 +1,17 @@
 <template>
   <header
-    class="fixed flex items-center justify-center w-full py-2 bg-gradient-to-b from-black to-transparent"
+    class="fixed z-40 flex items-center justify-center w-full h-10 bg-gradient-to-b from-black to-transparent"
     :class="{ 'bg-black': changeHeaderActiveHeader, 'bg-gradient-to-br': !changeHeaderActiveHeader }"
   >
-    <button class="flex justify-center flex-1" @click="isMenuOpen = true">
+    <button class="flex items-center justify-center flex-1 w-full h-full" @click="isMenuOpen = true">
       <IconMenu class="text-xl text-gray-100" />
     </button>
-    <div class="flex justify-center flex-1">
+    <NuxtLink to="/" class="flex items-center justify-center flex-1 h-full">
       <img class="h-7" src="~/assets/images/logo.jpg" alt="">
-    </div>
-    <div class="flex items-center justify-center flex-1 gap-3">
-      <IconInstagram class="text-xl text-gray-100" />
-      <IconWhatsapp class="text-xl text-gray-100" />
+    </NuxtLink>
+    <div class="flex items-center justify-center flex-1 h-full">
+      <a href="#" target="_blank" class="px-3"><IconInstagram class="text-xl text-gray-100" /></a>
+      <a href="#" target="_blank" class="px-3"><IconWhatsapp class="text-xl text-gray-100" /></a>
     </div>
     <TheMobileNav v-if="isMenuOpen" @close="isMenuOpen = false" />
   </header>
--- components/TheMobileNav.vue
+++ components/TheMobileNav.vue
@@ -5,18 +5,20 @@
     <button class="absolute p-2 top-4 right-4" @click="$emit('close')">
       <IconClose class="text-xl text-gray-100" />
     </button>
-    <img class="h-16 pb-4" src="~/assets/images/logo.jpg" alt="">
+    <NuxtLink to="/" class="flex items-center justify-center flex-1 h-full">
+      <img class="h-16 pb-4" src="~/assets/images/logo.jpg" alt="">
+    </NuxtLink>
     <NuxtLink
       v-for="({ text, to }, index) in mixinLinksLinks"
       :key="index"
       :to="to"
-      class="py-2 text-xl tracking-wider text-gray-100"
+      class="w-full py-2 text-xl tracking-wider text-center text-gray-100"
     >
       {{ text.toUpperCase() }}
     </NuxtLink>
-    <div class="flex items-center justify-center gap-3 pt-4">
-      <IconInstagram class="text-4xl text-gray-100" />
-      <IconWhatsapp class="text-4xl text-gray-100" />
+    <div class="flex items-center justify-center pt-2">
+      <IconInstagram class="p-1 text-4xl text-gray-100" />
+      <IconWhatsapp class="p-1 text-4xl text-gray-100" />
     </div>
   </nav>
 </template>
